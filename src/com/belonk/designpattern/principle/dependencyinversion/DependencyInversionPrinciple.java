c2965c66a6abf85699fc677c1f54c45421fef014
sun <belonk.sun@abcbooking.cn>
2020-05-24 17:05:54 +0800

    update

---
+++ src/com/belonk/designpattern/principle/dependencyinversion/DependencyInversionPrinciple.java
@@ -0,0 +1,22 @@
+package com.belonk.designpattern.principle.dependencyinversion;
+
+/**
+ * Created by sun on 2020/5/24.
+ *
+ * @author sunfuchang03@126.com
+ * @since 1.0
+ */
+public class DependencyInversionPrinciple {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+
+}
