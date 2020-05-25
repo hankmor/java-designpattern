a49d3e71ead24b0e00b6c5b3e0fa226348e60b80
sun <belonk.sun@abcbooking.cn>
2020-05-25 12:08:38 +0800

    添加里氏替换原则代码

---
+++ src/com/belonk/designpattern/principle/liskov/LiskovSubstitutionPrinciple.java
@@ -0,0 +1,87 @@
+package com.belonk.designpattern.principle.liskov;
+
+/**
+ * Created by sun on 2020/5/24.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class LiskovSubstitutionPrinciple {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	/**
+	 * 里氏替换原则：所有引用父类的地方，都可以换成子类。
+	 */
+	public static void main(String[] args) {
+		int a = 10, b = 2, c = 8;
+		// 创建子类实例
+		Minus minus = new Minus();
+		int   res   = minus.calc(a, b);
+		// 8
+		System.out.println(res);
+		res = minus.minusThenPlus(a, b, c);
+		// 16
+		System.out.println(res);
+
+		// 创建子类实例
+		Plus plus = new Plus();
+		plus.setMinus(minus);
+		// 12
+		res = plus.calc(a, b);
+		System.out.println(res);
+		// 16
+		res = plus.minusThenPlus(a, b, c);
+		System.out.println(res);
+	}
+}
+
+abstract class Math {
+	protected abstract int calc(int a, int b);
+
+	protected abstract int minusThenPlus(int a, int b, int c);
+}
+
+class Minus extends Math {
+	// 求减法
+	@Override
+	public int calc(int a, int b) {
+		return a - b;
+	}
+
+	// 求减法然后在求和
+	@Override
+	public int minusThenPlus(int a, int b, int c) {
+		return calc(a, b) + c;
+	}
+}
+
+class Plus extends Math {
+	// 依赖Minus类来求减法
+	private Minus minus;
+
+	// 求和实现
+	@Override
+	public int calc(int a, int b) {
+		return a + b;
+	}
+
+	// 求减法再求和
+	@Override
+	public int minusThenPlus(int a, int b, int c) {
+		return this.minus.calc(a, b) + c;
+	}
+
+	public void setMinus(Minus minus) {
+		this.minus = minus;
+	}
+}
---
+++ src/com/belonk/designpattern/principle/liskov/bad/NotLiskovSubstitutionPrinciple.java
@@ -0,0 +1,64 @@
+package com.belonk.designpattern.principle.liskov.bad;
+
+/**
+ * Created by sun on 2020/5/24.
+ *
+ * @author sunfuchang03@126.com
+ * @version 1.0
+ * @since 1.0
+ */
+public class NotLiskovSubstitutionPrinciple {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	public static void main(String[] args) {
+		/*
+		 * 违反了里氏替换原则，Math换成子类Calc，计算结果错误
+		 */
+
+		int   a     = 10, b = 2, c = 8;
+		Minus minus = new Minus();
+		int   res   = minus.calc(a, b);
+		// 8
+		System.out.println(res);
+		res = minus.minusThenPlus(a, b, c);
+		// 16
+		System.out.println(res);
+
+		minus = new Plus();
+		// 期望得到减法的结果8，实际上子类覆盖了父类的计算方法，得到的是12
+		res = minus.calc(a, b);
+		System.out.println(res);
+		// 期望得到16，实际上是20
+		res = minus.minusThenPlus(a, b, c);
+		System.out.println(res);
+	}
+}
+
+class Minus {
+	// 求减法
+	public int calc(int a, int b) {
+		return a - b;
+	}
+
+	// 求减法然后在求和
+	public int minusThenPlus(int a, int b, int c) {
+		return calc(a, b) + c;
+	}
+}
+
+class Plus extends Minus {
+	// 改为求和，错误的覆盖了父类的方法
+	@Override
+	public int calc(int a, int b) {
+		return a + b;
+	}
+}
