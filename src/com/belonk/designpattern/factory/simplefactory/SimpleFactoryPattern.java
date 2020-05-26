f9c190bb76bc8729bb9b18c8afc79dae73a4bebb
sun <belonk.sun@abcbooking.cn>
2020-05-26 16:53:52 +0800

    添加工厂模式代码

---
+++ src/com/belonk/designpattern/factory/simplefactory/SimpleFactoryPattern.java
@@ -0,0 +1,92 @@
+package com.belonk.designpattern.factory.simplefactory;
+
+/**
+ * Created by sun on 2020/5/26.
+ *
+ * @author sunfuchang03@126.com
+ * @since 1.0
+ */
+public class SimpleFactoryPattern {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	/**
+	 * 需求：店铺订购各种类型的牛奶
+	 */
+	public static void main(String[] args) {
+		Store store = new Store();
+		Milk  milk  = store.orderMilk("pure");
+		System.out.println("店铺订购了" + milk.getName());
+		milk = store.orderMilk("yogurt");
+		System.out.println("店铺订购了" + milk.getName());
+		milk = store.orderMilk("highCalcium");
+		System.out.println("店铺订购了" + milk.getName());
+
+		// 生产鲜奶，无法生产
+		milk = store.orderMilk("freshMilk");
+		System.out.println("店铺订购了" + milk.getName());
+	}
+}
+
+/**
+ * 现在，客户端只依赖一个工厂类，不直接依赖具体的牛奶
+ */
+class Store {
+	public Milk orderMilk(String name) {
+		return MilkOrderFactory.orderMilk(name);
+	}
+}
+
+/**
+ * 扭来订购工厂
+ */
+class MilkOrderFactory {
+	public static Milk orderMilk(String name) {
+		if ("pure".equals(name)) {
+			return new PureMilk();
+		} else if ("yogurt".equals(name)) {
+			return new Yogurt();
+		} else if ("highCalcium".equals(name)) {
+			return new HighCalciumMilk();
+		} else {
+			throw new RuntimeException("无法订购牛奶：" + name);
+		}
+	}
+}
+
+// 牛奶抽象类
+abstract class Milk {
+	abstract String getName();
+}
+
+// 纯牛奶
+class PureMilk extends Milk {
+	@Override
+	String getName() {
+		return "纯牛奶";
+	}
+}
+
+// 酸奶
+class Yogurt extends Milk {
+	@Override
+	String getName() {
+		return "酸奶";
+	}
+}
+
+// 高钙奶
+class HighCalciumMilk extends Milk {
+	@Override
+	String getName() {
+		return "高钙奶";
+	}
+}
