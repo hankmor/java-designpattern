754209d43db38918c8364b60aeb41054f031defe
sam <belonk@126.com>
2020-12-01 18:35:13 +0800

    add

---
+++ src/com/belonk/designpattern/composite/CompositePatternDemo.java
@@ -0,0 +1,25 @@
+package com.belonk.designpattern.composite;
+
+/**
+ * Created by sun on 2020/11/8.
+ *
+ * @author sunfuchang03@126.com
+ * @since 1.0
+ */
+public class CompositePatternDemo {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	public static void main(String[] args) {
+
+	}
+}
+
