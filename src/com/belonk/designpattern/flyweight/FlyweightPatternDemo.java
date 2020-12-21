a9a65b301bde0e7cd2a61e6f70e50249f286156b
huzhou <huzhou.v@gmail.com>
2020-12-21 12:10:52 +0800

    add flyweight pattern code.

--- src/com/belonk/designpattern/flyweight/FlyweightPatternDemo.java
+++ src/com/belonk/designpattern/flyweight/FlyweightPatternDemo.java
@@ -22,6 +22,14 @@ public class FlyweightPatternDemo {
 	//~ Methods
 
 	public static void main(String[] args) {
+		/*
+		 * 享元模式，提倡对象的细粒度控制和对象共享，需要充分分析出外部状态和内部状态
+		 * 外部状态：对象依赖的的一种标记，随着环境变化而改变，不可共享
+		 * 内部状态：享元对象内部，需要共享，不会改变
+		 *
+		 * 示例：共享网站，多个用户共同使用一套网站，支持新闻和博客两种形式。使用享元模式，分析出外部状态为网站用户，每个用户都有自己的
+		 * 网站。
+		 */
 		WebsiteFactory websiteFactory = new WebsiteFactory();
 		String type = "博客";
 		Website website = websiteFactory.getWebsite(type);
