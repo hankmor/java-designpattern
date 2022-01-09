91278b8e7e97abb89035c845c398d15f8808b2d8
huzhou <huzhou.v@gmail.com>
2022-01-09 22:51:45 +0800

    中介者模式代码

---
+++ src/main/java/com/belonk/designpattern/mediator/MediatorClient.java
@@ -0,0 +1,38 @@
+package com.belonk.designpattern.mediator;
+
+/**
+ * Created by sun on 2022/1/9.
+ *
+ * @author sunfuchang03@126.com
+ * @since 1.0
+ */
+public class MediatorClient {
+	//~ Static fields/constants/initializer
+
+
+	//~ Instance fields
+
+
+	//~ Constructors
+
+
+	//~ Methods
+
+	public static void main(String[] args) {
+		ConcreteMediator mediator = new ConcreteMediator();
+		ConcreteColleague1 colleague1 = new ConcreteColleague1(mediator);
+		ConcreteColleague2 colleague2 = new ConcreteColleague2(mediator);
+		mediator.setColleague1(colleague1);
+		mediator.setColleague2(colleague2);
+
+		colleague1.sendMessage("我是同事1");
+		colleague2.sendMessage("我是同事2");
+
+		/*
+		ConcreteColleague1 发送了消息: 我是同事1
+		ConcreteColleague2 收到了消息: 我是同事1
+		ConcreteColleague2 发送了消息: 我是同事2
+		ConcreteColleague1 收到了消息: 我是同事2
+		 */
+	}
+}
