5439e4e4d7708c645be7a6b006a13619589ad33e
huzhou <huzhou.v@gmail.com>
2022-01-11 18:15:54 +0800

    内部迭代器

--- src/main/java/com/belonk/designpattern/iterator/EnumerationDemo.java
+++ src/main/java/com/belonk/designpattern/iterator/EnumerationDemo.java
@@ -61,6 +61,6 @@ public class EnumerationDemo {
 
 		System.out.println("forEachRemaining");
 		// 内部迭代器，Vector内部没有使用迭代器，而ArrayList则使用增强for循环来处理
-		iterator.forEachRemaining(System.out::println);
+		ints.iterator().forEachRemaining(System.out::println);
 	}
 }
